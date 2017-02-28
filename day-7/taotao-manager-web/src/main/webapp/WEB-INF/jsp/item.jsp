5269ed7b2d0af9c8f709cf61492fa6196507bc60
zedosu <249044706@qq.com>
2017-02-28 18:23:03 +0800

    day-7: Redis集群 | Jedis客户端
    Redis集群。
    连接redis，使用Jedis客户端。单机版、集群版。
    业务逻辑中添加缓存：添加缓存不能影响正常的业务逻辑。

---
+++ day-7/taotao-manager-web/src/main/webapp/WEB-INF/jsp/item.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zh
+  Date: 2/25/2017
+  Time: 7:29 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${itemParam}
+</body>
+</html>
---
+++ day-7/taotao-rest/src/main/java/com/taotao/rest/service/impl/RedisServiceImpl.java
@@ -0,0 +1,36 @@
+package com.taotao.rest.service.impl;
+
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.ExceptionUtil;
+import com.taotao.rest.dao.JedisClient;
+import com.taotao.rest.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by zh on 2/28/2017.
+ */
+@Service
+public class RedisServiceImpl implements RedisService {
+
+    @Autowired
+    private JedisClient jedisClient;
+
+    @Value("${INDEX_CONTENT_REDIS_KEY}")
+    private String INDEX_CONTENT_REDIS_KEY;
+
+    @Override
+    public TaotaoResult syncContent(long categoryId) {
+        try {
+            jedisClient.hdel(INDEX_CONTENT_REDIS_KEY, categoryId + "");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return TaotaoResult.build(500, ExceptionUtil.getStackTrace(e));
+        }
+        return TaotaoResult.ok();
+    }
+
+    //todo synCategoryIndex
+
+}
